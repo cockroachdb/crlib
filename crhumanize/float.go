1094cb39adac890715ac73bf99c2f512dc44d8f2
Radu Berinde <radu@cockroachlabs.com>
2025-09-11 16:13:24 -0700

    crhumanize: add Percent
    Add a `Percent` function which prints a percentage and automatically
    decides to use 0 or 1 decimal digits.

--- crhumanize/float.go
+++ crhumanize/float.go
@@ -15,10 +15,13 @@
 package crhumanize
 
 import (
+	"math"
 	"strconv"
 	"strings"
 )
 
+// Float formats the given float with the specified number of decimal digits.
+// Trailing 0 decimals are stripped.
 func Float(value float64, decimalDigits int) SafeString {
 	s := strconv.FormatFloat(value, 'f', decimalDigits, 64)
 	s = stripTrailingZeroDecimals(s)
@@ -36,4 +39,30 @@ func stripTrailingZeroDecimals(s string) string {
 		s = s[:len(s)-1]
 	}
 	return s
+}
+
+// Percent formats (numerator/denominator) as a percentage. At most one decimal
+// digit is used (only when the integer part is a single digit). If denominator
+// is 0, returns the empty string.
+//
+// Values very close to 0 are formatted as ~0% to indicate that the value is
+// non-zero.
+//
+// Examples: "0.2%", "12%".
+func Percent[T Numeric](numerator, denominator T) SafeString {
+	if denominator == 0 {
+		return ""
+	}
+	if numerator == 0 {
+		return "0%"
+	}
+	value := (float64(numerator) / float64(denominator)) * 100
+	if math.Abs(value) < 0.05 {
+		return "~0%"
+	}
+	decimalDigits := 0
+	if math.Abs(value) < 9.95 {
+		decimalDigits = 1
+	}
+	return Float(value, decimalDigits) + "%"
 }
